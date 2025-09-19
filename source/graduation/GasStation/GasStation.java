6800866a1bc120ed4598f05e115e2e88f5e291f5
William Almeida <willliam.dev@gmail.com>
2025-09-19 12:35:37 -0300

    fix: total calories formula

---
+++ source/graduation/GasStation/GasStation.java
@@ -0,0 +1,5 @@
+package source.graduation.GasStation;
+
+public class GasStation {
+    
+}
--- source/graduation/Gym/Gym.java
+++ source/graduation/Gym/Gym.java
@@ -9,29 +9,29 @@ public class Gym {
     public static void main(String[] args) {
         int calories = 0, amount = 0, time_in_minutes = 0;
         do {
-            System.out.println("Quantos exercícios diferentes você fez hoje?");
+            System.out.print("Quantos exercícios diferentes você fez hoje?");
             amount = input.nextInt();
 
             if (amount < 1) {
-                System.out.println("Você precisa ter praticado 1 ou mais exercícios!!");
+                System.out.println("\nVocê precisa ter praticado 1 ou mais exercícios!!\n");
             }
         } while (amount < 1);
 
         for (int i = 0; i < amount; i++) {
             do {
-                System.out.println("Digite o tempo em minutos do " + (i + 1) + "° exercício:");
+                System.out.print("Digite o tempo em minutos do " + (i + 1) + "° exercício: ");
 
                 time_in_minutes += input.nextInt();
 
                 if (time_in_minutes < 1) {
-                    System.out.println("O tempo deve ser de 1 ou mais minutos!!");
+                    System.out.println("\nO tempo deve ser de 1 ou mais minutos!!\n");
                 }
             } while (time_in_minutes < 1);
         }
 
-        calories = time_in_minutes * amount * 5;
+        calories = time_in_minutes * 5;
 
-        System.out.println("Você gastou aproximadamente " + calories + " calorias no treino de hoje.");
+        System.out.println("\nVocê gastou aproximadamente " + calories + " calorias no treino de hoje.");
 
 
         input.close();
