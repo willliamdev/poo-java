ef96dc8319e9a117680b6ef803c3eaeb77c3f95c
William Almeida <willliam.dev@gmail.com>
2025-10-17 17:06:46 -0300

    feat: Add getter and setter methods to ensure encapsulation

--- source/graduation/pilares-poo/Animal.java
+++ source/graduation/pilares-poo/Animal.java
@@ -2,13 +2,13 @@ public class Animal {
     public String name;
     public int age;
     public String color;
-    
-    private String sex;
+
+    private char sex;
     private String noise;
     private String habitat;
     private int energy;
 
-    public Animal(String name, int age, String color, String sex, String noise, String habitat) {
+    public Animal(String name, int age, String color, char sex, String noise, String habitat) {
         this.name = name;
         this.age = age;
         this.color = color;
@@ -23,21 +23,71 @@ public class Animal {
         System.out.println(this.noise + "!" + this.noise + "!" + this.noise + "!");
     }
 
-    public void eat(){
-        if (energy <= 100){
+    public void eat() {
+        if (energy <= 100) {
             System.out.println(this.name + " já está com 100% de energia");
         }
-        energy+= 10;
+        energy += 10;
     }
 
-    private void move(){
+    private void move() {
         System.out.println(this.name + "está se movendo..");
-        energy-= 10;
+        energy -= 10;
     }
 
     private void hello() {
         makeNoise();
         System.out.println(this.name + "lhe dá as saudações!!");
+    }
+
+    // Getters e Setter pros atributos privados
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        if (sex != 'M' && sex != 'F') {
+            System.out.println("Valor de sexo inválido: " + sex + ". Deve ser 'M' ou 'F'.");
+            return;
+        }
+        this.sex = sex;
+        System.out.println("Sexo alterado para: " + this.sex);
+    }
+
+    public String getNoise() {
+        return noise;
+    }
+
+    public void setNoise(String noise) {
+        this.noise = noise;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        if (this.energy >= 100) {
+            System.out.println("Energia já está em " + 100 + "%");
+            return;
+        }
+
+        this.energy += energy;
+
+        // limitar em 100%
+        if (this.energy > 100) {
+            this.energy = 100;
+        }
+
     }
 
 }
